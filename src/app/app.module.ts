dfa292854968c6e11610b8ff6fa00a7415fbc0a0
acidrobin <54273015+acidrobin@users.noreply.github.com>
2023-07-07 16:17:26 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { ReactiveFormsModule } from "@angular/forms";
+import { BasicPopoverComponent } from "./demo-charts/basic-popover/basic-popover.component";
+import { CollapsableComponent } from "./demo-charts/collapsable/collapsable.component";
+import { ConnectorsComponent } from "./demo-charts/connectors/connectors.component";
+import { HomeChartsComponent } from "./demo-charts/home/home-charts.component";
+import { NgxTreantJsModule } from "@ahmed757/ngx-treant-js";
+import { ModalModule } from "ngx-bootstrap/modal";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicPopoverComponent,
+    CollapsableComponent,
+    ConnectorsComponent,
+    HomeChartsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxTreantJsModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/demo-charts/home/home-charts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "ngx-treant-demo-home-charts",
+  templateUrl: "./home-charts.component.html",
+  styleUrls: ["./home-charts.component.css"]
+})
+export class HomeChartsComponent implements OnInit {
+  title = "List of Charts";
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  openInNewTab(url) {
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
+}
